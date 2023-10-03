dda8679fb022dc49bf8ee4023ed731d0308da99a
RagunathRa <ragunath065@gmail.com>
2023-10-03 15:39:06 +0530

    222

---
+++ src/Components/password.js
@@ -0,0 +1,26 @@
+import React, {useRef} from 'react'
+
+const App = () => {
+ 
+const name = useRef(null)
+const password = useRef(null)
+
+const inpSubmit = (e) =>{
+e.preventDefault();
+console.log( password.current.value , name.current.value);
+
+}
+
+  return (
+    <div>
+      <form onSubmit={inpSubmit}>
+      <input type='text' ref={name}/>
+      <input type='password' ref={password}/>
+      <button type='submit'>submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
+
